f9cb59f882f6e99dfb0369564fbe16fad8dd3ffb
kseans8 <kseans8@gmail.com>
2018-02-08 18:12:49 -0500

    wagering stage implementation

--- src/game.js
+++ src/game.js
@@ -12,69 +12,47 @@ class Game extends React.Component {
     let player = new Hand();
     let dealer = new Hand();
 
-    deck.newDeck();
-    player.getCard(deck.deal());
-    dealer.getCard(deck.deal());
-    player.getCard(deck.deal());
-    dealer.getCard(deck.deal());
-
     this.state = {
       deck: deck,
       player: player,
       dealer: dealer,
-      playing: true,
+      stage: 'pregame',
+      wager: '',
+      properWager: false
     }
+    this.startNewGame = this.startNewGame.bind(this)
+    this.dealNewGame = this.dealNewGame.bind(this)
     this.hitMe = this.hitMe.bind(this)
     this.callStand = this.callStand.bind(this)
-    this.dealNewGame = this.dealNewGame.bind(this)
     this.determineWinner = this.determineWinner.bind(this)
+    this.gambleChange = this.gambleChange.bind(this)
   }
 
-  hitMe() {
-    let card = this.state.deck.deal()
-    this.state.player.getCard(card)
-    let newScore = this.state.player.calculateScore()
+  startNewGame(){
     this.setState({
-      score: newScore
+      stage: 'wagering',
+      wager: '',
+      properWager: false
     })
-    if (newScore > 20) {
-      this.callStand()
-    }
   }
 
-  callStand() {
-    let deck = this.state.deck;
-    let player = this.state.player;
-    let dealer = this.state.dealer;
-    while (dealer.calculateScore() < 17) {
-      dealer.getCard(deck.deal())
-    }
-    this.setState({
-      deck: deck,
-      dealer: dealer,
-      playing: false
-    })
-    this.determineWinner()
-  }
-
-  determineWinner() {
-    let player = this.state.player
-    let dealer = this.state.dealer
-
-    if (player.calculateScore() > 21 || dealer.calculateScore() === 21){
-      player.losses += 1;
-    } else if (dealer.calculateScore() > 21 || player.calculateScore() === 21 || player.calculateScore() > dealer.calculateScore()){
-      player.wins += 1;
-    } else if (dealer.calculateScore() > player.calculateScore()){
-      player.losses += 1;
-    } else if (dealer.calculateScore() === player.calculateScore()){
-      player.ties += 1;
+  gambleChange(e){
+    if (!isNaN(e.target.value) && e.target.value <= this.state.player.wallet) {
+      this.setState({
+        wager: e.target.value
+      })
     } else {
-      console.log("oh boy, something bad happened")
+      e.target.value = this.state.wager
     }
-    this.setState({
-      player: player
-    })
+    if (0 < e.target.value && e.target.value <= this.state.player.wallet) {
+      this.setState({
+        properWager: true
+      })
+    } else {
+      this.setState({
+        properWager: false
+      })
+    }
   }
 
   dealNewGame() {
@@ -95,46 +73,94 @@ class Game extends React.Component {
       deck: deck,
       player: player,
       dealer: dealer,
-      playing: true,
-      score: player.calculateScore(),
+      stage: 'playing',
+    })
+  }
+
+  hitMe() {
+    let card = this.state.deck.deal()
+    this.state.player.getCard(card)
+    let newScore = this.state.player.calculateScore()
+    this.setState({
+      score: newScore
+    })
+    if (newScore > 20) {
+      this.callStand()
+    }
+  }
+
+  callStand() {
+    let deck = this.state.deck;
+    let dealer = this.state.dealer;
+    while (dealer.calculateScore() < 17) {
+      dealer.getCard(deck.deal())
+    }
+    this.setState({
+      deck: deck,
+      dealer: dealer
+    })
+    this.determineWinner()
+  }
+
+  determineWinner() {
+    let player = this.state.player
+    let dealer = this.state.dealer
+
+    if (player.calculateScore() > 21 || dealer.calculateScore() === 21){
+      player.losses += 1;
+    } else if (dealer.calculateScore() > 21 || player.calculateScore() === 21 || player.calculateScore() > dealer.calculateScore()){
+      player.wins += 1;
+    } else if (dealer.calculateScore() > player.calculateScore()){
+      player.losses += 1;
+    } else if (dealer.calculateScore() === player.calculateScore()){
+      player.ties += 1;
+    } else {
+      console.log("oh boy, something bad happened")
+    }
+    this.setState({
+      player: player,
+      stage: 'postgame',
     })
   }
 
   render() {
     let playersCards = this.state.player.cards.map((card, index) => {
-      return(
-        <CardComponent
-          key={index}
-          card={card}
-          show={true}
-        />
-    )
-    })
-    let dealersCards = this.state.dealer.cards.map((card, index) => {
-      if (this.state.playing && index > 0) {
-        return(
-          <CardComponent
-            key={index}
-            card={card}
-            show={false}
-          />
-        )
-      } else {
+      if (this.state.stage === 'playing' || this.state.stage === 'postgame') {
         return(
           <CardComponent
             key={index}
             card={card}
             show={true}
-          />
+            />
         )
-
       }
     })
-    let dealerScore = <p>Dealers Score: {this.state.dealer.cards[0].value}</p>
-    if (!this.state.playing) {
-      dealerScore = (
-        <p>Dealers Score: {this.state.dealer.calculateScore()}</p>
-      )
+    let dealersCards = this.state.dealer.cards.map((card, index) => {
+      if (this.state.stage === 'playing' || this.state.stage === 'postgame') {
+        if (this.state.stage == 'playing' && index > 0) {
+          return(
+            <CardComponent
+              key={index}
+              card={card}
+              show={false}
+            />
+          )
+        } else {
+          return(
+            <CardComponent
+              key={index}
+              card={card}
+              show={true}
+            />
+          )
+        }
+      }
+    })
+    let dealerScore = <p></p>
+    if (this.state.stage === 'playing') {
+      dealerScore = <p>Dealers Score: {this.state.dealer.cards[0].value}</p>
+    } else if (this.state.stage === 'postgame') {
+      dealerScore = <p>Dealers Score: {this.state.dealer.calculateScore()}</p>
     }
     return(
       <div>
@@ -148,14 +174,25 @@ class Game extends React.Component {
         </div>
         <div className="game-details">
           <div className="actionButtons">
-            { this.state.playing ? <button onClick={this.hitMe}>Hit Me</button> : null }
-            { this.state.playing ? <button onClick={this.callStand}>Stand</button> : null }
-            { this.state.playing ? null : <button onClick={this.dealNewGame}>Deal</button> }
+            { this.state.stage === 'wagering' ?
+              <div>
+                <input
+                  type="text"
+                  value={this.state.wager}
+                  onChange={this.gambleChange}
+                  />
+                <button onClick={this.dealNewGame} disabled={!this.state.properWager}>gamble</button>
+              </div>
+              : null
+            }
+            { this.state.stage === 'playing'  ? <button onClick={this.hitMe}>Hit Me</button> : null }
+            { this.state.stage === 'playing' ? <button onClick={this.callStand}>Stand</button> : null }
+            { this.state.stage === 'postgame' || this.state.stage === 'pregame' ? <button onClick={this.startNewGame}>Start New Game</button> : null }
           </div>
           <div className="record">
+            { this.state.stage === 'playing' || this.state.stage === 'postgame' ? <p>Score: {this.state.player.calculateScore()}</p> : null}
+            { this.state.stage === 'playing' || this.state.stage === 'postgame' ? <div>{dealerScore}</div> : null }
             <p>Your Money: ${this.state.player.wallet}</p>
-            <p>Score: {this.state.player.calculateScore()}</p>
-            {dealerScore}
             <p>Wins: {this.state.player.wins}</p>
             <p>Losses: {this.state.player.losses}</p>
             <p>Ties: {this.state.player.ties}</p>
